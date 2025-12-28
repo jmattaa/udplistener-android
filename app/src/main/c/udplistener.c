a421e682bd32ad09f8e61ec2ba240c51f681b9d9
jmattaa <mattajonathan1@gmail.com>
2025-12-28 22:02:14 +0100

    add scrolling and coloring to output

--- app/src/main/c/udplistener.c
+++ app/src/main/c/udplistener.c
@@ -4,15 +4,19 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdio.h>
 
 #define BUF_SIZE 1024
+#define LOG_TAG "UDPListener"
 
 static int sock = -1;
 
 static char *udpbroadcast_listener(int port)
 {
-    struct sockaddr_in addr;
+    struct sockaddr_in addr, client_addr;
     static char buf[BUF_SIZE];
+    static char formatted_buf[BUF_SIZE + 256]; // extra space for metadata
 
     sock = socket(AF_INET, SOCK_DGRAM, 0); // UDP
     if (sock < 0)
@@ -36,12 +40,30 @@ static char *udpbroadcast_listener(int port)
     // and prints at the same time yk??
     while (1)
     {
-        int len = recvfrom(sock, buf, BUF_SIZE - 1, 0, NULL, NULL);
+        socklen_t client_len = sizeof(client_addr);
+        int len = recvfrom(sock, buf, BUF_SIZE - 1, 0, (struct sockaddr *)&client_addr, &client_len);
         if (len > 0)
         {
             buf[len] = '\0'; // terminate string
+            
+            // Get timestamp
+            time_t now;
+            struct tm *timeinfo;
+            char timestamp[64];
+            time(&now);
+            timeinfo = localtime(&now);
+            strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeinfo);
+            
+            // Get source IP
+            char *client_ip = inet_ntoa(client_addr.sin_addr);
+            
+            // Also return formatted string for UI display
+            snprintf(formatted_buf, sizeof(formatted_buf), 
+                "[%s] Received from %s\n%s", 
+                timestamp, client_ip, buf);
+            
             close(sock);
-            return buf;
+            return formatted_buf;
         }
     }
 }
--- app/src/main/java/com/jmattaa/udplistener/MainActivity.kt
+++ app/src/main/java/com/jmattaa/udplistener/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jmattaa.udplistener
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.text.Html
 import android.view.View
 import com.jmattaa.udplistener.databinding.ActivityMainBinding
 
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.stoplisten.isEnabled = false
+        binding.textView.movementMethod = ScrollingMovementMethod()
     }
 
     external fun listenUDP(port: Int): String
@@ -37,7 +40,10 @@ class MainActivity : AppCompatActivity() {
             while (listening) {
                 val msg = listenUDP(port)
                 runOnUiThread {
-                    binding.textView.append(msg + "\n")
+                    // Parse the message and add HTML colors
+                    val coloredMsg = formatMessageWithColors(msg)
+                    binding.textView.append(Html.fromHtml(coloredMsg, Html.FROM_HTML_MODE_COMPACT))
+                    binding.textView.append("\n")
                 }
             }
             runOnUiThread {
@@ -46,6 +52,40 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
+
+    private fun formatMessageWithColors(msg: String): String {
+    // Use regex to parse the formatted message and add HTML colors
+    val timestampPattern = "\\[(.*?)\\]".toRegex()
+    val receivedFromPattern = "Received from".toRegex()
+    val ipPortPattern = "(.*?):(\\d+)".toRegex()
+    
+    var result = msg
+    
+    // Color timestamp cyan
+    result = result.replace(timestampPattern) { match ->
+        "<font color='#00FFFF'><b>[${match.groupValues[1]}]</b></font>"
+    }
+    
+    // Color "Received from" green
+    result = result.replace(receivedFromPattern) { 
+        "<font color='#00FF00'><b>Received from</b></font>"
+    }
+    
+    // Color IP yellow and port blue
+    result = result.replace(ipPortPattern) { match ->
+        "<font color='#FFFF00'><b>${match.groupValues[1]}</b></font>:<font color='#0000FF'><b>${match.groupValues[2]}</b></font>"
+    }
+    
+    // Color the actual message data magenta (everything after the IP:port line)
+    val lines = result.split("\n")
+    if (lines.size > 1) {
+        val metadataLine = lines[0]
+        val dataLine = lines.subList(1, lines.size).joinToString("\n")
+        result = metadataLine + "\n<font color='#FF00FF'><b>$dataLine</b></font>"
+    }
+    
+    return result
+}
 
     public fun stoplistenClick(view: View) {
         listening = false

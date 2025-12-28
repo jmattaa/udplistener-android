5aafcff503f376d83995dcd90b6fc2a1a3fb3e79
jmattaa <mattajonathan1@gmail.com>
2025-12-28 21:41:14 +0100

    listening until stop button is pressed and recieving multiple packets

--- app/src/main/c/udplistener.c
+++ app/src/main/c/udplistener.c
@@ -1,19 +1,22 @@
 #include <jni.h>
 
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define BUF_SIZE 1024
 
-static char *udpbroadcast_listener(int port) {
-    int sock;
+static int sock = -1;
+
+static char *udpbroadcast_listener(int port)
+{
     struct sockaddr_in addr;
     static char buf[BUF_SIZE];
 
     sock = socket(AF_INET, SOCK_DGRAM, 0); // UDP
-    if (sock < 0) return "socket error";
+    if (sock < 0)
+        return "socket error";
 
     int broadcast = 1;
     setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
@@ -23,28 +26,39 @@ static char *udpbroadcast_listener(int port) {
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(port);
 
-    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
         close(sock);
         return "bind error";
     }
 
     // receive 1 packet TODO: async shi or idk but something that takes stuff
     // and prints at the same time yk??
-    while (1) {
+    while (1)
+    {
         int len = recvfrom(sock, buf, BUF_SIZE - 1, 0, NULL, NULL);
-        if (len > 0) {
-            buf[len] = '\0';  // terminate string
+        if (len > 0)
+        {
+            buf[len] = '\0'; // terminate string
             close(sock);
             return buf;
         }
     }
 }
 
-JNIEXPORT jstring JNICALL
-Java_com_jmattaa_udplistener_MainActivity_listenUDP(JNIEnv *env,
-                                                        jobject thiz,
-                                                        jint port)
+JNIEXPORT jstring JNICALL Java_com_jmattaa_udplistener_MainActivity_listenUDP(
+    JNIEnv *env, jobject thiz, jint port)
 {
     const char *msg = udpbroadcast_listener((int)port);
     return (*env)->NewStringUTF(env, msg);
+}
+
+JNIEXPORT void JNICALL
+Java_com_jmattaa_udplistener_MainActivity_stopUDP(JNIEnv *env, jobject thiz)
+{
+    if (sock >= 0)
+    {
+        close(sock);
+        sock = -1;
+    }
 }
--- app/src/main/java/com/jmattaa/udplistener/MainActivity.kt
+++ app/src/main/java/com/jmattaa/udplistener/MainActivity.kt
@@ -8,12 +8,14 @@ import com.jmattaa.udplistener.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    @Volatile private var listening = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.stoplisten.isEnabled = false
     }
 
     external fun listenUDP(port: Int): String
@@ -24,8 +26,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    public fun listenbtnClick(view: View) {
-        val port = binding.portInput.text.toString().toIntOrNull() ?: 0
-        binding.textView.text = listenUDP(port)
+    fun listenbtnClick(view: View) {
+        binding.textView.text = ""
+        val port = binding.portInput.text.toString().toIntOrNull() ?: return
+
+        listening = true
+        binding.stoplisten.isEnabled = true
+
+        Thread {
+            while (listening) {
+                val msg = listenUDP(port)
+                runOnUiThread {
+                    binding.textView.append(msg + "\n")
+                }
+            }
+            runOnUiThread {
+                binding.stoplisten.isEnabled = false
+            }
+        }.start()
+    }
+
+
+    public fun stoplistenClick(view: View) {
+        listening = false
+        binding.stoplisten.isEnabled = false
     }
 }
